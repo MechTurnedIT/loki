800838ab5fbe2e5af7ed70c3218c9c55a2e09d4b
MechIT <scorpionking.mamlesh@gmail.com>
2024-09-06 14:46:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "dev" {
     ami = "ami-0a2202cf4c36161a1"
     instance_type = "t2.micro"
+     key_name ="ireland"
     tags = {
       Name = "test-ec2"
     }
